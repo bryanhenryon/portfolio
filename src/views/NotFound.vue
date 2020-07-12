bb8119a975ccc9af7f0a92618a6f17dd8d85fed1
Bryan <b.henryon@outlook.fr>
2020-07-12 19:53:34 +0200

    Ajout de la page 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="not-found">
+    <div>
+      <h1>Erreur 404</h1>
+      <p>Cette page n'existe pas 😥</p>
+      <div id="return">
+        <router-link to="/">Revenir au site</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#not-found {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: 3em;
+    text-align: center;
+  }
+
+  p {
+    font-size: 1.3em;
+    text-align: center;
+  }
+
+  #return {
+    margin-top: 50px;
+    text-align: center;
+
+    a {
+      font-size: 1.3em;
+      border-bottom: 2px solid black;
+    }
+
+    a:hover {
+      border-bottom: 2px solid #79ad80;
+    }
+  }
+}
+</style>
